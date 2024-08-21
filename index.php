e73971a90daad5eec18641fa9a75b7bc47d3bdea
fannnn18 <irfanans762@gmail.com>
2024-08-21 16:30:01 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Special For My Lia</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm navbar-dark" style="background:black;">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#"><img src="image/saturnus.png" width="39px" alt=""></a>
+  
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="index.php?page=lover">Lover</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="index.php?page=pict">Our Pict</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="index.php?page=flowers">For u</a>
+    </li>
+  </ul>
+</nav>
+
+<?php
+
+include 'libs.php';
+
+?>
+
+</body>
+</html>
